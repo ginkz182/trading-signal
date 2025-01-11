313f60927eff8e94393de393ae619113b626e519
GinkF <ginkz182@hotmail.com>
2025-01-11 11:17:29 +0700

    api key

--- signal.js
+++ signal.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 
 class SignalCalculator {
   constructor(config = {}) {
-    this.exchange = new ccxt.binance();
+    this.exchange = new ccxt.binance({
+      apiKey: process.env.BINANCE_API_KEY,
+      secret: process.env.BINANCE_SECRET_KEY,
+    });
     this.lineToken = process.env.lineToken;
     this.symbols = config.symbols || [];
     this.timeframe = config.timeframe || '1d';
